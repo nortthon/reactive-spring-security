565fcd3a4708bd7d4bcbfaa15feacb18facf627f
Lucas Augusto <nortthon@gmail.com>
2018-08-25 01:33:16 -0300

    add facebook oauth login

--- src/main/java/io/github/nortthon/reactivespringsecurity/config/SecurityConfig.java
+++ src/main/java/io/github/nortthon/reactivespringsecurity/config/SecurityConfig.java
@@ -1,47 +1,18 @@
 package io.github.nortthon.reactivespringsecurity.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebFluxSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityWebFilterChain securitygWebFilterChain(final ServerHttpSecurity http) {
-        return http.authorizeExchange()
-                .pathMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                .anyExchange().authenticated()
-                .and().httpBasic()
-                .and().build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/home");
     }
 
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService(final PasswordEncoder passwordEncoder) {
-
-        UserDetails user = User
-                .withUsername("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User
-                .withUsername("admin")
-                .password(passwordEncoder.encode("password"))
-                .roles("ADMIN")
-                .build();
-
-        return new MapReactiveUserDetailsService(user, admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
--- src/main/java/io/github/nortthon/reactivespringsecurity/http/GreetController.java
+++ src/main/java/io/github/nortthon/reactivespringsecurity/http/GreetController.java
@@ -3,7 +3,6 @@ package io.github.nortthon.reactivespringsecurity.http;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 import java.security.Principal;
 
@@ -11,18 +10,8 @@ import java.security.Principal;
 @RestController
 public class GreetController {
 
-    @GetMapping
-    public Mono<String> greet(Mono<Principal> principal) {
-        return principal
-                .map(Principal::getName)
-                .map(name -> String.format("Hello, %s", name));
-    }
-
-    @GetMapping("/admin")
-    public Mono<String> greetAdmin(Mono<Principal> principal) {
-        return principal
-                .map(Principal::getName)
-                .map(name -> String.format("Admin access: %s", name))
-                .doOnSuccess(log::info);
+    @GetMapping("/home")
+    public String greetAdmin(Principal principal) {
+        return "Welcome " + principal.getName();
     }
 }
